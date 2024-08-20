eb9a36941b10b4f175fd568c0165634642fd7bf1
Ivan Calderon <iasuarez@worldoffice.com.co>
2024-08-20 12:07:56 -0500

    Se crea el componente que muestra la factura con sus modelos

---
+++ src/app/components/invoice/invoice.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { InvoiceService } from '../../services/invoice.service';
+import { Invoice } from '../../models/invoice';
+
+@Component({
+  selector: 'app-invoice',
+  standalone: true,
+  imports: [],
+  templateUrl: './invoice.component.html'
+})
+export class InvoiceComponent implements OnInit {
+
+  invoice!: Invoice;
+
+  constructor(private invoiceService: InvoiceService) { 
+    
+  }
+  
+  ngOnInit(): void {
+    this.invoice = this.invoiceService.getInvoice();
+  }
+
+}
