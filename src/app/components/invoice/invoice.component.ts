93cf0dd3d6e38c998762c73f5d564ae9e1d2e757
Ivan Calderon <iasuarez@worldoffice.com.co>
2024-08-21 09:59:49 -0500

    Se incluye el boton para incluir un nuevo item

--- src/app/components/invoice/invoice.component.ts
+++ src/app/components/invoice/invoice.component.ts
@@ -6,6 +6,8 @@ import { ClientViewComponent } from '../client-view/client-view.component';
 import { CompanyViewComponent } from '../company-view/company-view.component';
 import { ListItemsComponent } from '../list-items/list-items.component';
 import { TotalComponent } from '../total/total.component';
+import { FormItemComponent } from '../form-item/form-item.component';
+import { Item } from '../../models/item';
 
 @Component({
   selector: 'app-invoice',
@@ -14,7 +16,8 @@ import { TotalComponent } from '../total/total.component';
     ClientViewComponent, 
     CompanyViewComponent, 
     ListItemsComponent,
-  TotalComponent],
+    TotalComponent,
+    FormItemComponent],
   templateUrl: './invoice.component.html'
 })
 export class InvoiceComponent implements OnInit {
@@ -31,5 +34,9 @@ export class InvoiceComponent implements OnInit {
 
   removeItem(id: number){
     this.invoice = this.service.remove(id);
+  }
+
+  addItem(item: Item){
+    this.invoice = this.service.save(item);
   }
 }
--- src/app/services/invoice.service.ts
+++ src/app/services/invoice.service.ts
@@ -1,32 +1,40 @@
 import { Injectable } from '@angular/core';
 import { Invoice } from '../models/invoice';
 import { invoiceData } from '../data/invoice.data';
+import { Item } from '../models/item';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class InvoiceService {
+  private invoice: Invoice = invoiceData;
 
-  private invoice : Invoice = invoiceData;
+  constructor() {}
 
-  constructor() { }
-
-  getInvoice(): Invoice { 
+  getInvoice(): Invoice {
     const total = this.calculateTotal();
-    return {... this.invoice, total};
+    return { ...this.invoice, total };
   }
 
   calculateTotal() {
     //let total = 0;
     //this.invoice.items.forEach((item) => total += item.price * item.quantity);
     //return total;
-    return this.invoice.items.reduce((total, item) => total + (item.price * item.quantity), 0);
+    return this.invoice.items.reduce(
+      (total, item) => total + item.price * item.quantity,
+      0
+    );
   }
 
-  remove(id: number) : Invoice {
-    this.invoice.items = this.invoice.items.filter(item => item.id!= id);
+  remove(id: number): Invoice {
+    this.invoice.items = this.invoice.items.filter((item) => item.id != id);
     const total = this.calculateTotal();
-    return {... this.invoice, total};
+    return { ...this.invoice, total };
   }
-  
+
+  save(item: Item): Invoice {
+    this.invoice.items.push(item);
+    const total = this.calculateTotal();
+    return {...this.invoice, total };
+  }
 }
